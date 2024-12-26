635390a6f3f6e5f76b6a083a6098edf5d4b23844
Louis Benagha <labenagha@gmail.com>
2024-12-26 10:11:41 -0600

    added js to handle default client email btn

---
+++ static/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const EmailButton = document.getElementById('btn');
+    
+    EmailButton.addEventListener('click', () => {
+      window.location.href = 'mailto:arerepadelouisbenagha@outlook.com';
+    });
+});
